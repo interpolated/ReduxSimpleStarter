cc3afd0c1896177df5f8af1e81ba02dc6f2ce7e4
interpolated <rob.asher@outlook.com>
2016-08-04 11:37:47 +1000

    finished

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,31 @@
 import React from 'react';
+//import React, {Component} from 'react'; -- this will pull component off react
 
-const SearchBar = () => {
-	return <input type="text"/>
-};
-
+class SearchBar extends React.Component {
+  // just name function in es6 to set it to 
+  //render: render()
+  constructor(props){
+  	super(props);
+  	//this.state cannot be used outside out
+  	//constructor function
+  	this.state = {term:'asdf'};
+  }
+  render() {
+    return (
+      <div className="search-bar">
+      <input 
+        value = {this.state.term}
+        onChange = {this.onInputChange.bind(this)} />
+      <br/>
+      <span>{this.state.term}</span>
+      </div>    
+    )
+  }
+  // notice no comma separating methods
+  onInputChange(e) {
+  	this.setState({term:e.target.value});
+    this.props.onSearchTermChange(e.target.value);
+  }
+}
 export default SearchBar;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'React';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+import _ from 'lodash';
 
 const API_KEY = 'AIzaSyD4N5KXmkK9pzDU3m76cuwMC6nyW0JO93k';
+  
 
-const App = () => {
-	return(
-			<div>hi</div>
-		)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+  
+    this.videoSearch('surfboards')
+
+  }
+
+  videoSearch(term){
+    YTSearch(
+      {
+        key: API_KEY,
+        term: term
+      },
+      (videos) => {
+        this.setState({
+          videos:videos,
+          selectedVideo: videos[0]
+        })
+      }
+    );
+  }
+  render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+    		<SearchBar onSearchTermChange = {videoSearch} />
+        <VideoDetail video =  { this.state.selectedVideo }/>
+        <VideoList 
+          onVideoSelect = { video => this.setState({ selectedVideo:video }) }
+          videos={this.state.videos} />
+    	</div>
+    )
+  }
 }
-
 ReactDOM.render(<App/>, document.querySelector('.container'));
